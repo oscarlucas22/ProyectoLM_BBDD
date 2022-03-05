7742c7d69ae5642ff6f51eebbf11b4b184c59eba
oscarlucas22 <98739924+oscarlucas22@users.noreply.github.com>
2022-03-05 20:17:41 +0100

    actualizado: ejer 2 completado
    ejercicio 2 completado

--- principal.py
+++ principal.py
@@ -6,3 +6,11 @@ opcion = MostrarMenu()
 while opcion != 0:
     if opcion == 1:
         Mostrar_socios(db)
+    
+    elif opcion == 2:
+        print("-----------------------------------------------------")
+        print("Los nombres de las peliculas de la tabla PELICULAS")
+        print("-----------------------------------------------------")
+        nombre(db)
+        print()
+        Mostrar_NombrePelicula(db)
